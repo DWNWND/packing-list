ca1406eb5de183e299e25430b40ede32a663b297
Thea <theola51924@stud.noroff.no>
2024-08-13 17:56:00 +0200

    added location reload when pushing new items to array!

--- src/js/functionality/item/add.js
+++ src/js/functionality/item/add.js
@@ -10,27 +10,33 @@ export function addItem(luggageType) {
   const toiletriesCategoryCheck = document.getElementById(`${luggageType}toiletriesCategoryCheck`);
   const personalitemsCategoryCheck = document.getElementById(`${luggageType}personalitemsCategoryCheck`);
 
-  console.log(newItemForm)
+  console.log(newItemForm);
   newItemForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     if (clothesCategoryCheck.checked && !electronicsCategoryCheck.checked && !mischellaneousCategoryCheck.checked && !footwearCategoryCheck.checked && !toiletriesCategoryCheck.checked && !personalitemsCategoryCheck.checked) {
       pushToArray(`${luggageType}Luggage`, "clothes", newItemInput.value);
+      location.reload();
     }
     if (electronicsCategoryCheck.checked && !clothesCategoryCheck.checked && !mischellaneousCategoryCheck.checked && !footwearCategoryCheck.checked && !toiletriesCategoryCheck.checked && !personalitemsCategoryCheck.checked) {
       pushToArray(`${luggageType}Luggage`, "electronics", newItemInput.value);
+      location.reload();
     }
     if (mischellaneousCategoryCheck.checked && !clothesCategoryCheck.checked && !electronicsCategoryCheck.checked && !footwearCategoryCheck.checked && !toiletriesCategoryCheck.checked && !personalitemsCategoryCheck.checked) {
       pushToArray(`${luggageType}Luggage`, "mischellaneous", newItemInput.value);
+      location.reload();
     }
     if (footwearCategoryCheck.checked && !electronicsCategoryCheck.checked && !mischellaneousCategoryCheck.checked && !clothesCategoryCheck.checked && !toiletriesCategoryCheck.checked && !personalitemsCategoryCheck.checked) {
       pushToArray(`${luggageType}Luggage`, "footwear", newItemInput.value);
+      location.reload();
     }
     if (toiletriesCategoryCheck.checked && !clothesCategoryCheck.checked && !mischellaneousCategoryCheck.checked && !footwearCategoryCheck.checked && !electronicsCategoryCheck.checked && !personalitemsCategoryCheck.checked) {
       pushToArray(`${luggageType}Luggage`, "toiletries", newItemInput.value);
+      location.reload();
     }
     if (personalitemsCategoryCheck.checked && !clothesCategoryCheck.checked && !electronicsCategoryCheck.checked && !footwearCategoryCheck.checked && !toiletriesCategoryCheck.checked && !mischellaneousCategoryCheck.checked) {
       pushToArray(`${luggageType}Luggage`, "personalitems", newItemInput.value);
+      location.reload();
     }
     if (!clothesCategoryCheck.checked && !electronicsCategoryCheck.checked && !mischellaneousCategoryCheck.checked && !footwearCategoryCheck.checked && !toiletriesCategoryCheck.checked && !personalitemsCategoryCheck.checked) {
       alert("You must choose an item category."); //add these messages as user feedback instead
