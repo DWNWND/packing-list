c3018b53b6235865a0aeb322ebbd340a38d94cb4
Thea <theola51924@stud.noroff.no>
2024-08-13 16:39:04 +0200

    added all list categories

--- src/js/functionality/array/remove.js
+++ src/js/functionality/array/remove.js
@@ -7,37 +7,88 @@ export function removeFromArray(groupId, itemId, luggageType) {
   const clothesArr = parsedLuggage.clothes;
   const electronicsArr = parsedLuggage.electronics;
   const mischellaneousArr = parsedLuggage.mischellaneous;
+  const footwearArr = parsedLuggage.footwear;
+  const toiletriesArr = parsedLuggage.toiletries;
+  const personalitemsArr = parsedLuggage.personalitems;
 
   if (luggageType && groupId && itemId) {
     if (groupId.includes("clothes")) {
       const newClothesArr = clothesArr.filter((item) => item.id !== itemId);
 
-      const mainLuggage = {
+      const newLuggage = {
         clothes: newClothesArr,
         electronics: electronicsArr,
         mischellaneous: mischellaneousArr,
+        footwear: footwearArr,
+        toiletries: toiletriesArr,
+        personalitems: personalitemsArr,
       };
-      localStorage.setItem(luggageType, JSON.stringify(mainLuggage));
+      localStorage.setItem(luggageType, JSON.stringify(newLuggage));
     }
     if (groupId.includes("electronics")) {
       const newElectronicsArr = electronicsArr.filter((item) => item.id !== itemId);
 
-      const mainLuggage = {
+      const newLuggage = {
         clothes: clothesArr,
         electronics: newElectronicsArr,
         mischellaneous: mischellaneousArr,
+        footwear: footwearArr,
+        toiletries: toiletriesArr,
+        personalitems: personalitemsArr,
       };
-      localStorage.setItem(luggageType, JSON.stringify(mainLuggage));
+      localStorage.setItem(luggageType, JSON.stringify(newLuggage));
     }
     if (groupId.includes("mischellaneous")) {
       const newMischellaneousArr = mischellaneousArr.filter((item) => item.id !== itemId);
 
-      const mainLuggage = {
+      const newLuggage = {
         clothes: clothesArr,
         electronics: electronicsArr,
         mischellaneous: newMischellaneousArr,
+        footwear: footwearArr,
+        toiletries: toiletriesArr,
+        personalitems: personalitemsArr,
       };
-      localStorage.setItem(luggageType, JSON.stringify(mainLuggage));
+      localStorage.setItem(luggageType, JSON.stringify(newLuggage));
+    }
+    if (groupId.includes("footwear")) {
+      const newFootwearArr = footwearArr.filter((item) => item.id !== itemId);
+
+      const newLuggage = {
+        clothes: clothesArr,
+        electronics: electronicsArr,
+        mischellaneous: mischellaneousArr,
+        footwear: newFootwearArr,
+        toiletries: toiletriesArr,
+        personalitems: personalitemsArr,
+      };
+      localStorage.setItem(luggageType, JSON.stringify(newLuggage));
+    }
+    if (groupId.includes("toiletries")) {
+      const newToiletriesArr = toiletriesArr.filter((item) => item.id !== itemId);
+
+      const newLuggage = {
+        clothes: clothesArr,
+        electronics: electronicsArr,
+        mischellaneous: mischellaneousArr,
+        footwear: footwearArr,
+        toiletries: newToiletriesArr,
+        personalitems: personalitemsArr,
+      };
+      localStorage.setItem(luggageType, JSON.stringify(newLuggage));
+    }
+    if (groupId.includes("personalitems")) {
+      const newPersonalitemsArr = personalitemsArr.filter((item) => item.id !== itemId);
+
+      const newLuggage = {
+        clothes: clothesArr,
+        electronics: electronicsArr,
+        mischellaneous: mischellaneousArr,
+        footwear: footwearArr,
+        toiletries: toiletriesArr,
+        personalitems: newPersonalitemsArr,
+      };
+      localStorage.setItem(luggageType, JSON.stringify(newLuggage));
     }
   } else {
     throw new Error("Please provide a group id and item id to remove an item.");
