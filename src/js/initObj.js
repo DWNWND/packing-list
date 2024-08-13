2bef2133a5ef51736e95ad44245b1d0a93955bcc
Thea <theola51924@stud.noroff.no>
2024-08-13 19:18:03 +0200

    added my premade packing lists

--- src/js/initObj.js
+++ src/js/initObj.js
@@ -1,4 +1,5 @@
 //INT MAIN LUGGAGE
+export var mainelectronics = [];
 export var mainfootwear = [
   { item: "Sneakers (1)", checked: false },
   { item: "Running shoes (1)", checked: false },
@@ -6,7 +7,6 @@ export var mainfootwear = [
   { item: "Dress shoes (1)", checked: false },
   { item: "Indoor slippers", checked: false },
 ];
-export var mainelectronics = [];
 export var maintoiletries = [
   { item: "Tampons**", checked: false },
   { item: "Hair care (Redken)**", checked: false },
@@ -54,80 +54,100 @@ export var mainclothes = [
 ];
 
 //INT CABIN LUGGAGE
-export var cabinelectronics = [
-  { item: "charger", checked: false },
-  { item: "headset", checked: false },
-  { item: "laptop", checked: false },
-];
-export var cabinmischellaneous = [
-  { item: "book", checked: false },
-  { item: "snacks", checked: false },
-  { item: "toothbrush", checked: false },
-];
-export var cabinclothes = [
-  { item: "shorts", checked: false },
-  { item: "t-shirt", checked: false },
-  { item: "socks", checked: false },
-];
 export var cabinfootwear = [];
 export var cabinpersonalitems = [];
-export var cabintoiletries = [];
+export var cabintoiletries = [
+  { item: "Small bottle of face cream and body lotion", checked: false },
+  { item: "A small bottle of face wash", checked: false },
+  { item: "Toothbrush and toothpaste", checked: false },
+  { item: "Deodorants", checked: false },
+  { item: "Hair brush", checked: false },
+];
+export var cabinelectronics = [
+  { item: "Laptops + chargers", checked: false },
+  { item: "Work headset", checked: false },
+  { item: "GoPro + charger", checked: false },
+  { item: "Drone + charger", checked: false },
+  { item: "Microphone + charger", checked: false },
+  { item: "iPhone mount/selfie stick", checked: false },
+  { item: "Speaker", checked: false },
+];
+export var cabinmischellaneous = [
+  { item: "Important documents(visas, prescriptions, travel insurance, identification, import permits, health certificate)", checked: false },
+  { item: "Favourite pillow + neck pillow", checked: false },
+  { item: "A book or Kindle", checked: false },
+  { item: "Notebook + pen", checked: false },
+];
+export var cabinclothes = [
+  { item: "Scarf/also as extra blanket", checked: false },
+  { item: "Long trousers and sweater", checked: false },
+  { item: "One change of clothes", checked: false },
+  { item: "One change of underwear and socks", checked: false },
+];
 
 //INT PURSE LUGGAGE
-export var purseelectronics = [
-  { item: "charger", checked: false },
-  { item: "headset", checked: false },
-  { item: "laptop", checked: false },
-];
-export var pursemischellaneous = [
-  { item: "book", checked: false },
-  { item: "snacks", checked: false },
-  { item: "toothbrush", checked: false },
-];
-export var purseclothes = [
-  { item: "shorts", checked: false },
-  { item: "t-shirt", checked: false },
-  { item: "socks", checked: false },
-];
 export var pursefootwear = [];
 export var pursepersonalitems = [];
-export var pursetoiletries = [];
+export var purseelectronics = [
+  { item: "Charger (phones, headsets, ipad)", checked: false },
+  { item: "Noise-cancelling earbuds", checked: false },
+  { item: "iPads", checked: false },
+  { item: "Phones", checked: false },
+  { item: "Headsets", checked: false },
+  { item: "Powerbank", checked: false },
+];
+export var pursemischellaneous = [
+  { item: "Passports", checked: false },
+  { item: "Wallets + enough baht for taxi and import fees", checked: false },
+  { item: "Snacks", checked: false },
+  { item: "Snifferoos", checked: false },
+  { item: "Mints and gum", checked: false },
+  { item: "Favourite hand lotion**", checked: false },
+  { item: "Favourite chapstick/lip balm**", checked: false },
+  { item: "Eye blinds", checked: false },
+  { item: "Foam earplugs", checked: false },
+  { item: "Long warm socks", checked: false },
+];
+export var pursetoiletries = [
+  { item: "Hand sanitizer", checked: false },
+  { item: "Mini paper towels", checked: false },
+  { item: "Wet wipes", checked: false },
+  { item: "Tampons", checked: false },
+  { item: "Nail file", checked: false },
+  { item: "Chalk tablets", checked: false },
+  { item: "Ibuprofen and paracetamol", checked: false },
+  { item: "Sleeping pills", checked: false },
+  { item: "Melatonin", checked: false },
+  { item: "Allergy pills and nasal spray", checked: false },
+  { item: "Sinus de-swelling spray", checked: false },
+];
 
 //INT PERSONAL LUGGAGE
-export var personalelectronics = [
-  { item: "charger", checked: false },
-  { item: "headset", checked: false },
-  { item: "laptop", checked: false },
-];
-export var personalmischellaneous = [
-  { item: "book", checked: false },
-  { item: "snacks", checked: false },
-  { item: "toothbrush", checked: false },
-];
-export var personalclothes = [
-  { item: "shorts", checked: false },
-  { item: "t-shirt", checked: false },
-  { item: "socks", checked: false },
-];
+export var personalelectronics = [];
+export var personalmischellaneous = [];
+export var personalclothes = [];
 export var personalfootwear = [];
 export var personalpersonalitems = [];
 export var personaltoiletries = [];
 
 //INT PET CARRIER
-export var petelectronics = [
-  { item: "charger", checked: false },
-  { item: "headset", checked: false },
-  { item: "laptop", checked: false },
-];
-export var petmischellaneous = [
-  { item: "book", checked: false },
-  { item: "snacks", checked: false },
-  { item: "toothbrush", checked: false },
-];
 export var petclothes = [];
 export var petfootwear = [];
 export var petpersonalitems = [];
 export var pettoiletries = [];
+export var petelectronics = [{ item: "microchip scanner + charger", checked: false }, ,];
+export var petmischellaneous = [
+  { item: "Passport + all other important documents", checked: false },
+  { item: "Linen shirt or scarf to cover the carrier", checked: false },
+  { item: "Snacks", checked: false },
+  { item: "His own blanket", checked: false },
+  { item: "Food", checked: false },
+  { item: "Travel toilet", checked: false },
+  { item: "Travel harness and titer", checked: false },
+  { item: "Microchip reader + charge", checked: false },
+  { item: "Copy of owner passport", checked: false },
+  { item: "1500 baht for import fees", checked: false },
+];
 
 export function addRandomItemIds(array) {
   array.forEach((item) => {
