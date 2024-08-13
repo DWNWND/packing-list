53acc7880990be2f69d70a0e2edb13aaba323dbf
Thea <theola51924@stud.noroff.no>
2024-08-13 16:41:22 +0200

    added all list categories

--- src/js/functionality/item/add.js
+++ src/js/functionality/item/add.js
@@ -6,23 +6,36 @@ export function addItem(luggageType) {
   const clothesCategoryCheck = document.getElementById(`${luggageType}clothesCategoryCheck`);
   const electronicsCategoryCheck = document.getElementById(`${luggageType}electronicsCategoryCheck`);
   const mischellaneousCategoryCheck = document.getElementById(`${luggageType}mischellaneousCategoryCheck`);
+  const footwearCategoryCheck = document.getElementById(`${luggageType}footwearCategoryCheck`);
+  const toiletriesCategoryCheck = document.getElementById(`${luggageType}toiletriesCategoryCheck`);
+  const personalitemsCategoryCheck = document.getElementById(`${luggageType}personalitemsCategoryCheck`);
 
+  console.log(newItemForm)
   newItemForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    if (clothesCategoryCheck.checked && !electronicsCategoryCheck.checked && !mischellaneousCategoryCheck.checked) {
+    if (clothesCategoryCheck.checked && !electronicsCategoryCheck.checked && !mischellaneousCategoryCheck.checked && !footwearCategoryCheck.checked && !toiletriesCategoryCheck.checked && !personalitemsCategoryCheck.checked) {
       pushToArray(`${luggageType}Luggage`, "clothes", newItemInput.value);
     }
-    if (electronicsCategoryCheck.checked && !clothesCategoryCheck.checked && !mischellaneousCategoryCheck.checked) {
+    if (electronicsCategoryCheck.checked && !clothesCategoryCheck.checked && !mischellaneousCategoryCheck.checked && !footwearCategoryCheck.checked && !toiletriesCategoryCheck.checked && !personalitemsCategoryCheck.checked) {
       pushToArray(`${luggageType}Luggage`, "electronics", newItemInput.value);
     }
-    if (mischellaneousCategoryCheck.checked && !clothesCategoryCheck.checked && !electronicsCategoryCheck.checked) {
+    if (mischellaneousCategoryCheck.checked && !clothesCategoryCheck.checked && !electronicsCategoryCheck.checked && !footwearCategoryCheck.checked && !toiletriesCategoryCheck.checked && !personalitemsCategoryCheck.checked) {
       pushToArray(`${luggageType}Luggage`, "mischellaneous", newItemInput.value);
     }
-    if (!clothesCategoryCheck.checked && !electronicsCategoryCheck.checked && !mischellaneousCategoryCheck.checked) {
+    if (footwearCategoryCheck.checked && !electronicsCategoryCheck.checked && !mischellaneousCategoryCheck.checked && !clothesCategoryCheck.checked && !toiletriesCategoryCheck.checked && !personalitemsCategoryCheck.checked) {
+      pushToArray(`${luggageType}Luggage`, "footwear", newItemInput.value);
+    }
+    if (toiletriesCategoryCheck.checked && !clothesCategoryCheck.checked && !mischellaneousCategoryCheck.checked && !footwearCategoryCheck.checked && !electronicsCategoryCheck.checked && !personalitemsCategoryCheck.checked) {
+      pushToArray(`${luggageType}Luggage`, "toiletries", newItemInput.value);
+    }
+    if (personalitemsCategoryCheck.checked && !clothesCategoryCheck.checked && !electronicsCategoryCheck.checked && !footwearCategoryCheck.checked && !toiletriesCategoryCheck.checked && !mischellaneousCategoryCheck.checked) {
+      pushToArray(`${luggageType}Luggage`, "personalitems", newItemInput.value);
+    }
+    if (!clothesCategoryCheck.checked && !electronicsCategoryCheck.checked && !mischellaneousCategoryCheck.checked && !footwearCategoryCheck.checked && !toiletriesCategoryCheck.checked && !personalitemsCategoryCheck.checked) {
       alert("You must choose an item category."); //add these messages as user feedback instead
     } else if ((clothesCategoryCheck.checked && electronicsCategoryCheck.checked && !mischellaneousCategoryCheck.checked) || (clothesCategoryCheck.checked && mischellaneousCategoryCheck.checked && !electronicsCategoryCheck.checked) || (electronicsCategoryCheck.checked && mischellaneousCategoryCheck.checked && !clothesCategoryCheck.checked)) {
-      alert("You can only choose one item category."); //add these messages as user feedback instead
+      alert("You can only choose one item category."); //add these messages as user feedback instead - fix this to work for all
     }
   });
 }
